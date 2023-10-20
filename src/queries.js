a4d7a2407e5be3c9f007111f136c2cd4e7859d91
Jacob Foote <jacobjfoote01@gmail.com>
2023-10-20 16:19:28 -0600

    commit

--- src/queries.js
+++ src/queries.js
@@ -2,34 +2,88 @@ import { Op } from 'sequelize';
 import { Animal, Human } from './model.js';
 
 // Get the human with the primary key 2
-export const query1 = null;
+export const query1 = await Human.findByPk(2)
+console.log(query1)
 
 // Get the first animal whose species is "fish"
-export const query2 = null;
+export const query2 = await Animal.findOne({ where: {species: 'fish' }})
+console.log(query2)
 
 // Get all animals belonging to the human with primary key 5
-export const query3 = null;
+export const query3 = await Animal.findAll({ where: { human_id: 5 }}) 
+console.log(query3)
 
 // Get all animals born in a year greater than (but not equal to) 2015.
-export const query4 = null;
+export const query4 = await Animal.findAll({
+    where: {
+        [Op.gt]:[
+            { birth_year:{[Op.gt]: 2015 }}
+            ]
+        }
+}) 
+console.log(query4)
 
 // Get all the humans with first names that start with "J"
-export const query5 = null;
+export const query5 = await Human.findAll({ 
+    where: {
+        [Op.startsWith]:[
+            { fname: 'J%' }
+        ]
+    }
+})
+console.log(query5)
 
 // Get all the animals who don't have a birth year
-export const query6 = null;
+export const query6 = await Animal.findAll({ 
+    where: {
+        [Op.is]:[
+            { birth_year: null }
+        ]
+    }
+})
+console.log(query6)
 
 // Get all the animals with species "fish" OR "rabbit"
-export const query7 = null;
+export const query7 = await Animal.findAll({ 
+    where: {
+        [Op.or]:[
+            { 
+            species: fish, 
+            species: rabbit 
+            }
+        ]
+    }
+})
+console.log(query7)
 
 // Get all the humans who DON'T have an email address that contains "gmail"
-export const query8 = null;
+export const query8 = await Human.findAll({ 
+    where: {
+        [Op.ne]:[ 
+            {
+            email: gmail.com
+            }
+        ]
+    }
+})
+console.log(query8)
 
 // Continue reading the instructions before you move on!
 
 // Print a directory of humans and their animals
-export async function printHumansAndAnimals() {}
+export async function printHumansAndAnimals() {
+    const directory = await Human.findAll({ include: Animal })
+
+    for (const directory of directories) {
+        const pet = directory.Animal
+        if (pet) {
+            console.log (directory.Human, pet.human_id )
+        } 
+    }
+}
 
 // Return a Set containing the full names of all humans
 // with animals of the given species.
-export async function getHumansByAnimalSpecies(species) {}
+export async function getHumansByAnimalSpecies(species) {
+    
+}
